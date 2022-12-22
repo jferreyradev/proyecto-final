ca224ccf3f4a0e4b5844444da78ff8fe389a1ffb
José R. Ferreyra <jferreyra.dev@gmail.com>
2022-12-21 23:46:16 -0300

    implementacion router

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <base-login />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import BaseLogin from '@/components/BaseLogin.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    BaseLogin
+  }
+}
+</script>
---
+++ src/views/RegistroUsuarioView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <base-registro-usuario />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import BaseRegistroUsuario from '@/components/BaseRegistroUsuario.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    BaseRegistroUsuario
+  }
+}
+</script>
